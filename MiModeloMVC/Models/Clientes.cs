e004a22a43f7c0f40f60405361fa316769ab2183
Edgard Marquez <edgardma@gmail.com>
2018-04-11 05:37:32 -0500

    Agregar archivos de proyecto.

---
+++ MiModeloMVC/Models/Clientes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MiModeloMVC.Models
+{
+    public class Clientes
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string nombre { get; set; }
+
+        [Display (Name = "Fecha de alta")]
+        [DataType (DataType.Date)]
+        [DisplayFormat (DataFormatString = "{0:dd-MM-YYYY}", ApplyFormatInEditMode = true)]
+        public DateTime FechaAlta { get; set; }
+
+        [Range (18, 75)]
+        public int edad { get; set; }
+    }
+
+    public class EmpDBContext : DbContext
+    {
+        public EmpDBContext()
+        {
+
+        }
+
+        public DbSet<Clientes> Clientes { get; set; }
+    }
+}
