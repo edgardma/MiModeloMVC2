0501cd1fbca92b0ea16a8f3ebd506a3a74b60bd9
Edgard Marquez <edgardma@gmail.com>
2018-04-19 05:51:13 -0500

    Otra aplicacion de ejemplo

---
+++ MiPrimeraAplicacion/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimeraAplicacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Mi primera aplicación ASP.NET MVC 5.";
+        }
+    }
+}
