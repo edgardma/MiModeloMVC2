63ccf122e062e0e5c73a7289f9bbc4cca1c8528f
Edgard Marquez <edgardma@gmail.com>
2018-04-19 05:53:40 -0500

    Proyecto de ejemplo

---
+++ MiControladorMVC/Controllers/ClientesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiControladorMVC.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Buscar(string nombre)
+        {
+            var input = Server.HtmlEncode(nombre);
+
+            return Content(input);
+        }
+
+        [HttpGet]
+        public ActionResult Buscar()
+        {
+            var input = "Este es un selector HTTP get.";
+
+            return Content(input);
+        }
+    }
+}
---
+++ MiControladorMVC/Controllers/ProveedoresController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiControladorMVC.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        // GET: Proveedores
+        public string TodosLosProveedores()
+        {
+            return @"<ul>
+                     <li>Alcampo</li>
+                     <li>Carrefur</li>
+                     <li>Mecadona</li>
+                     <li>Gadis</li>
+                     </ul>";
+        }
+    }
+}
