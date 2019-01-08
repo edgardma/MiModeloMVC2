11c574cf84906015602c7af8225ca8bbbf326e64
Edgard Marquez <edgardma@gmail.com>
2019-01-07 23:13:57 -0500

    Ejemplo API RESTfull DotNetCore y Swagger

---
+++ EjemploAPI/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace EjemploAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Tutorial para Swagger
+            // https://geeks.ms/jlguerrero/2018/04/16/openapi-en-mi-api-rest-net-core-con-swashbuckle/
+
+            // Tutorial para comentarios XML para Swagger
+            // https://somostechies.com/agregando-swagger-a-asp-net-web-api-2/#.XC_NwM221hE
+
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory + @"\bin\";
+            var commentsFileName = Assembly.GetExecutingAssembly().GetName().Name + ".xml";
+            var commentsFile = Path.Combine(baseDirectory, commentsFileName);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Version = "v1",
+                        Title = "PruebaAPI - OpenAPI",
+                        Description = "API to expose Taskin logic",
+                        TermsOfService = "https://www.google.com.pe"
+                    }
+                );
+                options.IncludeXmlComments(commentsFile);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Taskin V1");
+                });
+        }
+    }
+}
