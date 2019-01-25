ca736fdc5a010e8ba1414f8bd7822119f4a96f7d
Edgard Marquez <edgardma@gmail.com>
2019-01-24 22:44:01 -0500

    Ejemplo 1.5 y 1.6 Examen 70-483
    Ejemplo 1.5 y 1.6 Examen 70-483

---
+++ Examen70-483.Ejemplos/Chapter1/Listing15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Examen70_483.Ejemplos.Chapter1.Listing15
+{
+    public static class Program
+    {
+        [ThreadStatic]
+        public static int _field;
+
+        public static void Main()
+        {
+            new Thread(() =>
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread A: {0}", _field);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread B: {0}", _field);
+                }
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
--- Examen70-483.Ejemplos/Program.cs
+++ Examen70-483.Ejemplos/Program.cs
@@ -13,7 +13,9 @@ namespace Examen70_483.Ejemplos
             //Chapter1.Listing11.Program.Main();
             //Chapter1.Listing12.Program.Main();
             //Chapter1.Listing13.Program.Main();
-            Chapter1.Listing14.Program.Main();
+            //Chapter1.Listing14.Program.Main();
+            //Chapter1.Listing15.Program.Main();
+            Chapter1.Listing16.Program.Main();
 
             Console.ReadLine();
         }
