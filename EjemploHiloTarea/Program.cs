0949cda2ed9419e093d0b29f3fae8333a5746f76
Edgar M. Marquez Aylas <edgardma@gmail.com>
2019-11-01 15:30:38 -0500

    Ejemplo de Hilos y Tareas

--- EjemploHiloTarea/Program.cs
+++ EjemploHiloTarea/Program.cs
@@ -1,19 +1,22 @@
-using System;
+﻿using System;
 
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApplicationHilos
+namespace EjemploHiloTarea
 {
-    class Program
+    public class Program
     {
         // http://artesanos.de/software/2011/10/04/tareas-vs-hilos-en-c-3/
         static void Main(string[] args)
         {
+            Console.WriteLine("======================================");
+            Console.WriteLine("Procesar tareas");
             ProcesarTareas();
+            Console.WriteLine("======================================");
+            Console.WriteLine("Procesar hilos");
+            ProcesarHilos();
+            Console.ReadLine();
         }
 
         static void ProcesarHilos()
@@ -22,7 +25,7 @@ namespace ConsoleApplicationHilos
             for (int i = 0; i < 100000; i++)
             {
                 int temp = i;
-                Thread t = new Thread(() => method1(temp));
+                Thread t = new Thread(() => Method1(temp));
                 t.Start();
             }
 
@@ -33,16 +36,16 @@ namespace ConsoleApplicationHilos
                                fin.ToString("yyyy/MM/dd HH:mm:ss.fffffff"),
                                diff.Hours, diff.Minutes, diff.Seconds,
                                diff.Milliseconds);
-            Console.ReadLine();
         }
 
-        static void ProcesarTareas() 
+        static void ProcesarTareas()
         {
             DateTime inicio = DateTime.Now;
             for (int i = 0; i < 100000; i++)
             {
                 int temp = i;
-                Task t = Task.Factory.StartNew(() => method1(temp));
+                Task t = Task.Factory.StartNew(() => Method1(temp));
+                //t.Start();
             }
             DateTime fin = DateTime.Now;
             TimeSpan diff = fin - inicio;
@@ -51,15 +54,19 @@ namespace ConsoleApplicationHilos
                                 fin.ToString("yyyy/MM/dd HH:mm:ss.fffffff"),
                                 diff.Hours, diff.Minutes, diff.Seconds,
                                 diff.Milliseconds);
-            Console.ReadLine();
         }
 
-        static void method1(int i)
+        static void Method1(int i)
         {
             for (int j = 0; j < 10000; j++)
             {
                 double d = 45345 / 6546 * 7989 / 0.2254;  //cualquier procesamiento...
             }
+        }
+
+        private Program()
+        {
+
         }
     }
 }
